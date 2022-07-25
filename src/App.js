f0b2762975f5ec21792e75f819f19fb69e356333
L. Yasril Imam <60500658+yasrilimam98@users.noreply.github.com>
2022-07-25 21:53:05 +0700

    update component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
-import NavbarComponent from "./components/NavbarComponent";
+import { Col, Row, Container } from "react-bootstrap";
+import { Hasil, ListCategories, NavbarComponent } from "./components";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
